d0273db151e447ae5d3cdf0b8b9280a2d0c2ae86
paulwilliams <paalpwmd>
2020-08-03 20:20:05 -0500

    finish studio 3

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -3,27 +3,61 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-crew',
   templateUrl: './crew.component.html',
-  styleUrls: ['./crew.component.css']
+  styleUrls: ['./crew.component.css'],
 })
 export class CrewComponent implements OnInit {
-
   inCrew: boolean = false;
   crew: object[] = [];
 
   candidates: object[] = [
-    {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
-    {name: "Mae Jemison", photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg'},
-    {name: "Ellen Ochoa", photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg'},
-    {name: "Frederick Gregory", photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg'},
-    {name: "Guion Bluford", photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg'},
-    {name: "Kjell Lindgren", photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg'},
-    {name: "Jeanette Epps", photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg'}
+    {
+      name: 'Sally Ride',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/sally-ride.jpg',
+    },
+    {
+      name: 'Mae Jemison',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg',
+    },
+    {
+      name: 'Ellen Ochoa',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg',
+    },
+    {
+      name: 'Frederick Gregory',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg',
+    },
+    {
+      name: 'Guion Bluford',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg',
+    },
+    {
+      name: 'Kjell Lindgren',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg',
+    },
+    {
+      name: 'Jeanette Epps',
+      photo:
+        'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg',
+    },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   // Code the 'addCrewMember' function here:
-
+  addCrewMember(candidate: object) {
+    if (this.crew.includes(candidate)) {
+      let asIndex = this.crew.indexOf(candidate);
+      this.crew.splice(asIndex, 1);
+    } else if (this.crew.length < 3) {
+      this.crew.push(candidate);
+    }
+  }
 }
--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,31 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { __core_private_testing_placeholder__ } from '@angular/core/testing';
 
 @Component({
   selector: 'app-equipment',
   templateUrl: './equipment.component.html',
-  styleUrls: ['./equipment.component.css']
+  styleUrls: ['./equipment.component.css'],
 })
 export class EquipmentComponent implements OnInit {
-   equipmentItems: object[] = [
-       {name: 'Duct Tape', mass: 0.5},
-       {name: 'Space Camera', mass: 20},
-       {name: 'Food', mass: 150},
-       {name: 'Oxygen Tanks', mass: 400},
-       {name: 'AE-35 Unit', mass: 5},
-       {name: 'ISS Supplies', mass: 800},
-       {name: 'Water', mass: 250},
-       {name: 'Satellite', mass: 1200},
-       {name: 'R2 Unit', mass: 32}
-   ];
-   cargoHold: object[] = [];
-   cargoMass: number = 0;
-   maximumAllowedMass: number = 2000;
-   maxItems: number = 10;
+  equipmentItems: object[] = [
+    { name: 'Duct Tape', mass: 0.5 },
+    { name: 'Space Camera', mass: 20 },
+    { name: 'Food', mass: 150 },
+    { name: 'Oxygen Tanks', mass: 400 },
+    { name: 'AE-35 Unit', mass: 5 },
+    { name: 'ISS Supplies', mass: 800 },
+    { name: 'Water', mass: 250 },
+    { name: 'Satellite', mass: 1200 },
+    { name: 'R2 Unit', mass: 32 },
+  ];
+  cargoHold: object[] = [];
+  cargoMass: number = 0;
+  maximumAllowedMass: number = 2000;
+  maxItems: number = 10;
 
-   constructor() { }
+  constructor() {}
 
-   ngOnInit() { }
+  ngOnInit() {}
 
-   // Code your addItem function here:
-   
+  // Code your addItem function here:
+  addItem(equipment: object) {
+    this.cargoHold.push(equipment);
+    this.cargoMass += equipment.mass;
+    if (this.cargoMass + equipment.mass > this.maximumAllowedMass) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
